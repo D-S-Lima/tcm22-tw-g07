73f3e43a23000deab27d7ab34fe7f4509c979094
D-S-Lima <82171400+D-S-Lima@users.noreply.github.com>
2023-01-15 16:58:19 +0000

    ggg

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 var Ball = document.getElementById('Ball')
 var Hoop = document.getElementById('Hoop')
-var myScore;
 function startGame() {
-  Ball.gravity = 0.05;
+  
 }
-function shoot2(n) {
-  Ball.gravity = n;
+function Game2() {
+  
 }
