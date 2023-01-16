f81afbadbce813a2e0df85fd2889b61fa77e3c99
D-S-Lima <82171400+D-S-Lima@users.noreply.github.com>
2023-01-16 00:36:32 +0000

    d

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var Ball = document.getElementById('Ball')
 var Hoop = document.getElementById('Hoop')
+var x= 0
 function Game() {
-  
+    while(x<300){
+      var element = Ball
+      element.style.margin = "50px"
+    }
 }
