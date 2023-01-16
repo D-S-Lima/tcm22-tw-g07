5e38a53f948190e39c6119cb43e9fc95ff36d20b
D-S-Lima <82171400+D-S-Lima@users.noreply.github.com>
2023-01-16 00:38:45 +0000

    vv

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var Ball = document.getElementById('Ball')
-var Hoop = document.getElementById('Hoop')
-var x= 0
 function Game() {
+    var Ball = document.getElementById('Ball')
+    var Hoop = document.getElementById('Hoop')
+    var x= 0
     while(x<300){
       var element = Ball
       element.style.margin = "50px"
