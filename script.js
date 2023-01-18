474846aa685d9ce80575052f59470fc4bac828a1
D-S-Lima <82171400+D-S-Lima@users.noreply.github.com>
2023-01-18 18:32:06 +0000

    Top Btn

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let mybutton = document.getElementById("Top");
 window.onscroll = function () { Scroll() };
 function Scroll() {
   let mybutton = document.getElementById("Top");
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
