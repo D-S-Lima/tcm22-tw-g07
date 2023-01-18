9f44d0b141b04c1308d4f826d967313baab90a85
D-S-Lima <82171400+D-S-Lima@users.noreply.github.com>
2023-01-18 19:37:32 +0000

    titulos

--- script.js
+++ script.js
@@ -33,5 +33,45 @@ function Text() {
       btn.value = "Play";
       btn.innerHTML = "Play";
   }
+}
 
+var i = 0;
+var x;
+titulo(i);
+
+function titulo(i) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this, i);
+        }
+    };
+    xmlhttp.open("GET", "cd_catalog.xml", true);
+    xmlhttp.send();
+}
+
+function myFunction(xml, i) {
+    var xmlDoc = xml.responseXML; 
+    x = xmlDoc.getElementsByTagName("CD");
+    document.getElementById("titulos").innerHTML =
+    "Artist: " +
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+    "<br>Title: " +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "<br>Year: " + 
+    x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue;
+}
+
+function next() {
+if (i < x.length-1) {
+  i++;
+  titulo(i);
+  }
+}
+
+function previous() {
+if (i > 0) {
+  i--;
+  titulo(i);
+  }
 }
