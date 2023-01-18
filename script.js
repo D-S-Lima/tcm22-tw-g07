741a031a77d19e0c8821a645dc2c26e9018ad642
D-S-Lima <82171400+D-S-Lima@users.noreply.github.com>
2023-01-18 20:17:04 +0000

    Glow2

--- script.js
+++ script.js
@@ -34,29 +34,3 @@ function Text() {
       btn.innerHTML = "Play";
   }
 }
-
-
-function loadXMLDoc() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
-    }
-  };
-  xmlhttp.open("GET", "titulos.xml", true);
-  xmlhttp.send();
-}
-function myFunction(xml) {
-  var i;
-  var xmlDoc = xml.responseXML;
-  var table="<tr><th>Name</th><th>Ano</th></tr>";
-  var x = xmlDoc.getElementsByTagName("List");
-  for (i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("ANO")[0].childNodes[0].nodeValue +
-    "</td></tr>";
-  }
-  document.getElementById("titulos").innerHTML = table;
-}
