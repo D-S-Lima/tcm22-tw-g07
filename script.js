e375cb909922c049247a4fe0a437de3746ceb527
D-S-Lima <82171400+D-S-Lima@users.noreply.github.com>
2023-01-18 18:05:57 +0000

    Scroll

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+let mybutton = document.getElementById("Top");
+window.onscroll = function () { Scroll() };
 
-function playPause(video1) { 
+function Scroll() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function GoTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+function playPause(video1) {
   var myVideo = document.getElementById("video1");
-  if (myVideo.paused) 
+  if (myVideo.paused)
     myVideo.play()
-  else 
+  else
     myVideo.pause()
-    } 
+} 
